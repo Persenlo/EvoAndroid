f9a7ec9472ed5a4f02a798ae4888912dc4c5f8b2
Persenlo <73698745+Persenlo@users.noreply.github.com>
2022-08-02 20:00:11 +0800

    引入抖音SDK

--- app/src/main/java/com/qxy/evoandroid/MainActivity.java
+++ app/src/main/java/com/qxy/evoandroid/MainActivity.java
@@ -1,10 +1,9 @@
 package com.qxy.evoandroid;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/qxy/evoandroid/UserLogin/LoginViewModel.java
@@ -0,0 +1,22 @@
+package com.qxy.evoandroid.UserLogin;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+/**
+ * 登录模块ViewModel
+ * @author Persenlo
+ *
+ */
+public class LoginViewModel extends AndroidViewModel {
+
+    public LoginViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public void login(){
+
+    }
+}
