78d52f3e0e018e5ea74e474fefa990bbde48acf8
wongguo <1361820008@qq.com>
2022-08-10 21:42:27 +0800

    二次提交代码

---
+++ app/src/main/java/com/qxy/evoandroid/list/ListActivity.java
@@ -0,0 +1,6 @@
+package com.qxy.evoandroid.list;
+
+import com.qxy.evoandroid.BaseActivity;
+
+public class ListActivity extends BaseActivity {
+}
---
+++ app/src/main/java/com/qxy/evoandroid/list/ListViewModel.java
@@ -0,0 +1,12 @@
+package com.qxy.evoandroid.list;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class ListViewModel extends AndroidViewModel {
+    public ListViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
